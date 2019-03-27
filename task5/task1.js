cdfc2ad7777c77fa9b84934495bdd82afa41054c
5029444 <48426266+5029444@users.noreply.github.com>
2019-03-27 06:06:26 +0300

    some more code formating

--- task5/task1.js
+++ task5/task1.js
@@ -1,76 +1,76 @@
-var Calculator = (function() {
-  var _curState = 0;
-  return {
+var Calculator = (function () {
+    var _curState = 0;
+    return {
 
-    getResult: function getResult () {
-      return _curState;
-      },
+        getResult: function getResult() {
+            return _curState;
+        },
 
-    reset: function reset () {
-      return (_curState=0);
-      },
- 
-    add: function add (num) {
-      num = +num;
-        if ( (typeof(num) == "undefined") || (isNaN(num)) ){
-          num = 0;           
-          };
-      _curState = _curState + num;
-        return add ;
-      },
+        reset: function reset() {
+            return (_curState = 0);
+        },
 
-    subtract: function subtract (num) {
-      num = +num;
-        if ( (typeof(num) == "undefined") || (isNaN(num)) ){
-          num = 0;           
-          };
-     _curState = _curState - num;
-        return subtract ;
-      },
+        add: function add(num) {
+            num = +num;
+            if ((typeof (num) == "undefined") || (isNaN(num))) {
+                num = 0;
+            };
+            _curState = _curState + num;
+            return add;
+        },
 
-    divide: function divide (num) {
-      num = +num;
-        if ( (typeof(num) == "undefined") || (isNaN(num)) || (num == 0) ){
-          num = 1;           
-          };
+        subtract: function subtract(num) {
+            num = +num;
+            if ((typeof (num) == "undefined") || (isNaN(num))) {
+                num = 0;
+            };
+            _curState = _curState - num;
+            return subtract;
+        },
+
+        divide: function divide(num) {
+            num = +num;
+            if ((typeof (num) == "undefined") || (isNaN(num)) || (num == 0)) {
+                num = 1;
+            };
             if (_curState == 0) {
-            _curState = num
-            num = 1;
+                _curState = num
+                num = 1;
             };
 
-      _curState = (_curState / num);  
-        return divide ;
-      },
+            _curState = (_curState / num);
+            return divide;
+        },
 
-    multiply: function multiply (num) {
-      num = +num;
-        if ( (typeof(num) == "undefined") || (isNaN(num)) || (num == 0) ){
-        num = 0;           
-        };
-          if (_curState == 0) {
-          _curState = num
-          num = 1;
-          };
-      _curState = (_curState * num);  
-      return multiply ;
-      },
+        multiply: function multiply(num) {
+            num = +num;
+            if ((typeof (num) == "undefined") || (isNaN(num)) || (num == 0)) {
+                num = 0;
+            };
+            if (_curState == 0) {
+                _curState = num
+                num = 1;
+            };
+            _curState = (_curState * num);
+            return multiply;
+        },
     };
-  }) ();
+})();
 
 // действия для проверки взяты из задания. ожидаемые результаты в комментариях к действиям тоже из задания
 // для сравнения с реальными
-Calculator.reset ();
+Calculator.reset();
 Calculator.add(4);
 Calculator.subtract(1);
-console.log (Calculator.getResult ()); //3
+console.log(Calculator.getResult()); //3
 
-Calculator.reset ();
+Calculator.reset();
 Calculator.add(4)(1);
 console.log(Calculator.getResult()); //5
 Calculator.subtract(1)(1)(1)(2);
 console.log(Calculator.getResult()); //0
 
-Calculator.reset ();
+Calculator.reset();
 Calculator.add()();
 console.log(Calculator.getResult()); // 0
 Calculator.subtract()()()();
