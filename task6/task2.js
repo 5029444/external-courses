ed4a6659b86106d7e64a1786f6fb66eb4787a716
5029444 <48426266+5029444@users.noreply.github.com>
2019-03-28 06:24:55 +0300

    some logic correction

--- task6/task2.js
+++ task6/task2.js
@@ -1,19 +1,20 @@
 var arrTest = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-console.log(sameAsSome(arrTest));
+console.log(sameAsSome(arrTest, callback));
 
+//пользовательская проверка
+function callback(item, i, array) {
+    return item > 0
+};
 
 function sameAsSome(array, callback) {
+
     for (var i = 0; i < array.length; i++) {
-        callback(array[i], i, array)
-    };
-// в callback проверяем, например, что какой-то элемент массива меньше 0
-    function callback(item, i, array) {
-        if (item < 0) {
-            return bool = true;
-        } else {
-            return bool = false;
+        item = array[i];
+
+        if ( callback(item, i, array) ) {
+            return true;
         };
+        return false;
     };
-    return bool;
 };
