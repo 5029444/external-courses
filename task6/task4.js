ba1177d320a16cbb9284fb1a33cec361f3510611
5029444 <48426266+5029444@users.noreply.github.com>
2019-03-28 07:03:33 +0300

    task ready

---
+++ task6/task4.js
@@ -0,0 +1,20 @@
+var arrTest = [1, 2, 3, 4, 5, 6, -7, 8, 9, 10];
+
+console.log(sameAsFilter(arrTest, callback));
+
+//пользовательская проверка
+function callback(item, i, array) {
+    return item > 0
+};
+
+function sameAsFilter(array, callback) {
+    var arrFiltered = [];
+    for (var i = 0; i < array.length; i++) {
+        item = array[i];
+
+        if (callback(item, i, array))  {
+            arrFiltered.push(array[i]);;
+        };
+    };
+    return arrFiltered;
+};
