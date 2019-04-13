d0adccdf90ff02953edaf79701d2e08524038eb9
5029444 <48426266+5029444@users.noreply.github.com>
2019-04-13 12:36:46 +0300

    fixed missin 0

--- task4/task13.js
+++ task4/task13.js
@@ -3,6 +3,6 @@ var rnd = 0;
 console.log ( rnd100 () );
 
 function rnd100() {
-	var rnd = Math.floor( Math.random() * (10 + 1) );
+	var rnd = Math.floor( Math.random() * (100 + 1) );
 	return rnd;
 };
